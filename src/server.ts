0b1dd96c18fc8a2f3a20defb40d148b12366655f
PRAZERES SANTOS Ilton Karly <ikprazeressantos@europe.corp.altran.com>
2020-04-20 11:38:49 +0100

    Configurando o eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express, { request, response } from "express";
+import express from 'express';
+
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ mensage: "Hello GoStack" });
+app.get('/', (request, response) => {
+  response.json({ mensage: 'Hello GoStack' });
 });
 
 app.listen(3333, () => {
-  console.log("🚀 Server started on port 3333");
+  console.log('🚀 Server started on port 3333');
 });
