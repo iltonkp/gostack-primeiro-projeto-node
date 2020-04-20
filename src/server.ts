c47abc6e2c9bc0d6a94ecca7aded54344426717b
PRAZERES SANTOS Ilton Karly <ikprazeressantos@europe.corp.altran.com>
2020-04-20 11:55:16 +0100

    Importando arquivos ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ mensage: 'Hello GoStack' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
